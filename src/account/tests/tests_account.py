3bac50786e395eea0ec9fe94a080c5749a3c8312
olyflower <2013.olga.yudina@gmail.com>
2023-02-20 14:33:52 +0200

    Add model Artist, add tests

--- src/account/tests/tests_account.py
+++ src/account/tests/tests_account.py
@@ -9,14 +9,22 @@ class TestAccountModel(TestCase):
         self.test_account = sample_account(
             email="abc@gmail.com", password="12345", first_name="Mary", last_name="Jons"
         )
+        self.test_account.get_short_name()
+        self.test_account.get_full_name()
 
     def tearDown(self):
         self.test_account.delete()
 
-    def test_first_name_limit(self):
+    def test_first_name_max_length(self):
         with self.assertRaises(ValidationError):
             sample_account(email="abc@gmail.com", password="12345", first_name="A" * 1000)
 
-    def test_last_name_limit(self):
+    def test_last_name_max_length(self):
         with self.assertRaises(ValidationError):
             sample_account(email="abc@gmail.com", password="12345", last_name="A" * 1000)
+
+    def test_get_short_name(self):
+        self.assertEqual(self.test_account.get_short_name(), "Mary")
+
+    def test_get_full_name(self):
+        self.assertEqual(self.test_account.get_full_name(), "Mary Jons")
