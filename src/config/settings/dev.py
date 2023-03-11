dae2244d2eba32f92bad144526a62fd5416107d6
olyflower <2013.olga.yudina@gmail.com>
2023-03-11 20:20:50 +0200

    hw_28

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -1,3 +1,5 @@
+import os
+
 from config.settings.base import *  # noqa
 
 DEBUG = True
@@ -6,12 +8,39 @@ SECRET_KEY = "django-secret-key"
 
 ALLOWED_HOSTS = []
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if os.environ.get("GITHUB_WORKFLOW"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "postgres",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "0.0.0.0",
+            "PORT": 5432,
+        }
     }
-}
-
+else:
+    DATABASES = {
+        # "default_local": {
+        #     "ENGINE": "django.db.backends.postgresql",
+        #     "NAME": "my_database",
+        #     "USER": "postgres",
+        #     "PASSWORD": "admin",
+        #     "HOST": "localhost",
+        #     "PORT": 5432,
+        # },
+        # "default_sqlite": {
+        #     "ENGINE": "django.db.backends.sqlite3",
+        #     "NAME": BASE_DIR / "db.sqlite3",
+        # },
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST"),
+            "PORT": os.environ.get("POSTGRES_PORT"),
+        },
+    }
 
 STATIC_URL = "static/"
--- src/music/urls.py
+++ src/music/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from music.views import GetTracksView
+from music.views import (GenreDetailView, GetGenresView, GetTracksView,
+                         TrackDetailView)
 
 app_name = "music"
 
 urlpatterns = [
-    path("", GetTracksView.as_view(), name="get_tracks"),
+    path("track_list/", GetTracksView.as_view(), name="get_tracks"),
+    path("genre_list/", GetGenresView.as_view(), name="get_genres"),
+    path("genre/<int:pk>/", GenreDetailView.as_view(), name="genre_detail"),
+    path("track/<int:pk>/", TrackDetailView.as_view(), name="track_detail"),
 ]
