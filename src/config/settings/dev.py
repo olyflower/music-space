1eccbb298857f81039c4a93975df25e8dc5c22f0
olyflower <2013.olga.yudina@gmail.com>
2023-03-13 18:05:23 +0200

    Add changes in index, settings

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -29,11 +29,11 @@ else:
         #     "HOST": "localhost",
         #     "PORT": 5432,
         # },
-        "default": {
+        "default_sqlite": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
         },
-        "default_docker": {
+        "default": {
             "ENGINE": "django.db.backends.postgresql",
             "NAME": os.environ.get("POSTGRES_DB"),
             "USER": os.environ.get("POSTGRES_USER"),
