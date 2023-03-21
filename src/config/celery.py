9bcee2619eedfe2d8781514d355436d14182d27b
olyflower <2013.olga.yudina@gmail.com>
2023-03-21 09:20:48 +0200

    celery-flower

--- src/config/celery.py
+++ src/config/celery.py
@@ -1,8 +1,5 @@
 from celery import Celery
 
 app = Celery("music")
-app.config_from_object(
-    "django.conf.settings",
-    namespace="CELERY"
-)
+app.config_from_object("django.conf.settings", namespace="CELERY")
 app.autodiscover_tasks()
