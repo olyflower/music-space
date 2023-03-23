101b3643c14c4f76902a08ac6c64086c647ce2aa
olyflower <2013.olga.yudina@gmail.com>
2023-03-23 21:31:44 +0200

    add edit_user_profile

--- src/music/urls.py
+++ src/music/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
 
-from music.views import (GenreDetailView, GetGenresView, GetTracksView,
-                         TrackDetailView, albums, artists, genres, tracks)
+from music.views import (GenreDetailView, GetGenresView, GetPlaylistView,
+                         GetTracksView, PlaylistDetailView, TrackDetailView,
+                         albums, artists, genres, tracks)
 
 app_name = "music"
 
 urlpatterns = [
     path("track_list/", GetTracksView.as_view(), name="get_tracks"),
     path("genre_list/", GetGenresView.as_view(), name="get_genres"),
+    path("playlist_list/", GetPlaylistView.as_view(), name="get_playlist"),
     path("genre/<int:pk>/", GenreDetailView.as_view(), name="genre_detail"),
     path("track/<int:pk>/", TrackDetailView.as_view(), name="track_detail"),
+    path("playlist/<int:pk>/", PlaylistDetailView.as_view(), name="playlist_detail"),
     path("generate-artists/<int:count>/", artists, name="generate_artists"),
     path("generate-genres", genres, name="generate_genres"),
     path("generate-albums/<int:count>/", albums, name="generate_albums"),
